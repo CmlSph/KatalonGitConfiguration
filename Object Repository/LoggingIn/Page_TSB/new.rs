2ac479af9cc394c5fe7e123f0886ecdf01b104fc
Cemal.Sipahioglu <Cemal.Sipahioglu@192.168.178.129>
2022-05-16 16:22:53 +0200

    bbb

---
+++ Object Repository/LoggingIn/Page_TSB/new.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>new</name>
+   <tag></tag>
+   <elementGuidId>52f38068-bb70-45e2-9041-18112ff8c1f6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>ccc</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>ccc</value>
+   </webElementProperties>
+</WebElementEntity>
